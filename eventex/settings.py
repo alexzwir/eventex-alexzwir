ed9b378b5d2399fe68b461ebebe79c98cf0026ee
Alex Zwir <alexzwir@gmail.com>
2016-01-03 13:28:42 -0200

    Arrumando o settings.py com as configurações do bd ('dburl')

--- eventex/settings.py
+++ eventex/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from decouple import config
+from dj_database_url import parse as bdurl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
